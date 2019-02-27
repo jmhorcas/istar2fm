2fc9b29ce72b130a579d0f9b717e89531b0b8f49
José Miguel Horcas Aguilera <horcas@lcc.uma.es>
2019-02-27 13:11:25 +0100

    Add constraints in CNF

--- fm2splot.py
+++ fm2splot.py
@@ -8,7 +8,7 @@ Script to generate a S.P.L.O.T. model in the SXFM format from a .xmi feature mod
 import argparse
 import datetime
 import featureModel
-
+import sympy
 
 EOL = '\n'
 TAB = '\t'
@@ -55,7 +55,7 @@ def add_feature(fm, feature, n_tabs, root=False, id=0):
     if feature.groupMultiplicity is not None:
         lower = feature.groupMultiplicity.lower
         upper = feature.groupMultiplicity.upper
-        if lower == -1: lower = 1
+        if lower == -1: lower = 1                   # limitation of SPLOT.
         if upper == -1: upper = '*'
         #code += tabs + TAB + ':g (_id' + str(id) + ') [' + str(lower) + ',' + str(upper) + ']' + EOL    # Con IDs
         code += tabs + TAB + ':g [' + str(lower) + ',' + str(upper) + ']' + EOL                         # Sin IDs
@@ -79,9 +79,54 @@ def feature_tree(fm):
 
     return tree
 
+CONSTRAINTS_RESERVED = ['implies', 'or', 'and', 'not']
+
+def extract_symbols(constraint):
+    symbols = constraint.split()
+    symbols = [x for x in symbols if x not in CONSTRAINTS_RESERVED]
+    symbols = [x.replace('(', '').replace(')', '') for x in symbols]
+    return symbols
+
+def from_constraint_to_formula(constraint):
+    symbols = constraint.split()
+    symbols = map(lambda x: '>>' if x == 'implies' else x, symbols)
+    symbols = map(lambda x: '|' if x == 'or' else x, symbols)
+    symbols = map(lambda x: '&' if x == 'and' else x, symbols)
+    symbols = map(lambda x: '~' if x == 'not' else x, symbols)
+    return "".join(list(symbols))
+
+def to_cnf(constraint):
+    #print("C: " + constraint)
+    symbols = extract_symbols(constraint)
+    formula_str = from_constraint_to_formula(constraint)
+
+    #print("F: " + formula_str)
+    #print("S: " + str(symbols))
+    x = sympy.symbols(symbols)
+    formula = sympy.to_cnf(formula_str)
+
+    splot_formula = str(formula).replace('|', 'or')
+    return splot_formula
+
+def __imprimir_constraint(fm, c):
+    for f in fm.get_features():
+        if f.id in c:
+            c = c.replace(f.id, f.name)
+    print("F: " + c)
 
 def constraints(fm):
     code = '<constraints>' + EOL
+
+    constraints_cnf = set()
+    for c in fm.get_constraints():
+        __imprimir_constraint(fm, c.code)
+        constraints_cnf.add(to_cnf(c.code))
+
+    count = 1
+    for c in constraints_cnf:
+        code += 'C' + str(count) + ': ' + c + EOL
+        count += 1
+
     code += '</constraints>' + EOL
     return code
 
